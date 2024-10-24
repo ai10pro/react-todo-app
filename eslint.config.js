2573b1ff35f6e35d27a854ccf60698d51ecc755d
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 09:54:53 +0900

    ESLintのカスタマイズ

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn', // 追加
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
