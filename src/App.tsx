fec1a776beb174a81a7c390f56e57f2605c2f3cc
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 10:12:53 +0900

    ESLintでTailwindの自動並び替え

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ const App = () => {
     setCount(newCount);
   };
   return (
-    <div className="mx-4 md:mx-auto mt-10 max-w-2xl"> 
-      <h1 className="font-bold text-2xl mb-4 text-violet-600">TodoApp</h1>
-      <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white py-0.5 px-4 rounded-full cursor-not-allowed"
-        onClick={countUp}>count is {count}</button>
+    <div className="mx-4 mt-10 max-w-2xl md:mx-auto">
+      <h1 className="mb-4 text-2xl font-bold text-violet-500">TodoApp</h1>
+      <button
+        className="cursor-not-allowed rounded-full bg-blue-500 px-4 py-0.5 text-white hover:bg-blue-700"
+        onClick={countUp}
+      >
+        count is {count}
+      </button>
     </div>
   );
 };
