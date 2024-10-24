af42ff1c0344b799b5b285629c036a4d6d10349c
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 10:29:55 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import { useState } from "react";
-import WelcomeMessage from "./WelcomeMessage";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const countUp = () => {
+  const countUp1 = () => {
+    // 追加
     const newCount = count + 1;
+    setCount(newCount);
+  };
+  const countUp10 = () => {
+    // 追加
+    const newCount = count + 10;
     setCount(newCount);
   };
   return (
     <div className="mx-4 mt-10 max-w-2xl md:mx-auto">
-      <h1 className="mb-4 text-2xl font-bold text-violet-500">TodoApp</h1>
-      <WelcomeMessage name="萱島ウサギ" uncompletedCount={6} />
-      <button
-        className="cursor-not-allowed rounded-full bg-blue-500 px-4 py-0.5 text-white hover:bg-blue-700"
-        onClick={countUp}
-      >
-        count is {count}
-      </button>
+      <h1 className="mb-6 text-2xl font-bold">TodoApp</h1>
+      <div className="mb-2">
+        <p>
+          現在のカウント値は
+          <span className="text-xl font-bold text-blue-500"> {count} </span>です
+        </p>
+      </div>
+      <div className="flex space-x-2">
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={countUp1}
+        >
+          1 Up
+        </button>
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={countUp10}
+        >
+          10 Up
+        </button>
+      </div>
     </div>
   );
 };
