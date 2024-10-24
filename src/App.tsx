32bd28ae7c0341b38b2641b4e0726ab3735da348
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 10:18:28 +0900

    update 1 file and create 1 file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import WelcomeMessage from "./WelcomeMessage";
 
 const App = () => {
-  const hoge = "Hoge";
   const [count, setCount] = useState(0);
   const countUp = () => {
     const newCount = count + 1;
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div className="mx-4 mt-10 max-w-2xl md:mx-auto">
       <h1 className="mb-4 text-2xl font-bold text-violet-500">TodoApp</h1>
+      <WelcomeMessage name="萱島ウサギ" />
       <button
         className="cursor-not-allowed rounded-full bg-blue-500 px-4 py-0.5 text-white hover:bg-blue-700"
         onClick={countUp}
