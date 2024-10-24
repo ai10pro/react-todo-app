c0ba178aa5d069f98ff7a3db469293e62e7de37c
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 10:33:27 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const reset = () => setCount(0);
   const countUp1 = () => setCount(count + 1);
   const countUp10 = () => setCount(count + 10);
+  const degrease = () => setCount(count - 1);
+  const degrease10 = () => setCount(count - 10);
   return (
     <div className="mx-4 mt-10 max-w-2xl md:mx-auto">
       <h1 className="mb-6 text-2xl font-bold">TodoApp</h1>
@@ -25,6 +28,24 @@ const App = () => {
           onClick={countUp10}
         >
           10 Up
+        </button>
+        <button
+          className="rounded-md bg-red-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={reset}
+        >
+          Reset
+        </button>
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={degrease}
+        >
+          1 down
+        </button>
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={degrease10}
+        >
+          10 down
         </button>
       </div>
     </div>
