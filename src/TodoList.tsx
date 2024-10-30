f82ea050ef19bb30a9779721c2c85c14b597c3b7
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-30 23:45:08 +0900

    feat: create TodoList.tsx

---
+++ src/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Todo } from './types'
+
+type Props = {
+    todos: Todo[]
+};
+
+export const TodoList = (props: Props) => {
+    const todos = props.todos;
+    return (
+        <div className="space-y-1">
+            <div>
+                {todos[0].name} 優先度 {todos[0].priority}
+            </div>
+            <div>
+                {todos[1].name} 優先度 {todos[1].priority}
+            </div>
+            <div>
+                {todos[2].name} 優先度 {todos[2].priority}
+            </div>
+        </div>
+    )
+}
