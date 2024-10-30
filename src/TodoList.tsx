282c4fb5dc60d793bb7205c498a89568c8d59342
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-31 00:06:36 +0900

    update TodoList.tsx

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -6,18 +6,31 @@ type Props = {
 };
 
 const TodoList = (props: Props) => {
-    const todos = props.todos;
+    const todos = [...props.todos].sort((a, b) => {
+        if (a.isDone !== b.isDone) {
+            return a.isDone ? 1 : -1;
+        } else {
+            const aDeadline = a.deadline ? a.deadline.getTime() : 0;
+            const bDeadline = b.deadline ? b.deadline.getTime() : 0;
+            return aDeadline - bDeadline;
+        }
+    });
+
+    if (todos.length === 0) {
+        return (
+            <div className="text-red-500">
+                やることがありません！
+            </div>
+        )
+    }
+
     return (
         <div className="space-y-1">
-            <div>
-                {todos[0].name} 優先度: {todos[0].priority}
-            </div>
-            <div>
-                {todos[1].name} 優先度: {todos[1].priority}
-            </div>
-            <div>
-                {todos[2].name} 優先度: {todos[2].priority}
-            </div>
+            {todos.map((todo) => (
+                <div key={todo.id}>
+                    {todo.name} 優先度: {todo.priority}
+                </div>
+            ))}
         </div>
     );
 };
