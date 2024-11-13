1a9839a7affa2c012bdc0355bd9c1cac484c0729
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-11-13 21:27:12 +0900

    create 1 file and update 3 files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     port: 3000, // デフォルトのポートを3000に設定
     strictPort: false,
